77c52cf03eb34fdf594610357827316078e5cf41
Jeppe BLicher Tarp <raider.j@gmail.com>
2013-12-03 13:23:07 +0100

    HexGrid finished

--- cells/static/js/hexgrid.class.js
+++ cells/static/js/hexgrid.class.js
@@ -13,12 +13,10 @@ HexGrid = function(tile_size, tile_cutoff, edge_size, grid_offset, orientation)
 	this.grid_offset = grid_offset;
 	
 	this.tile_deadzone = [this.tile_size[0] / 2, this.tile_cutoff / 2];
-	this.tile_livezone = [this.tile_size[0], this.tile_size[1] - this.tile_cutoff];
+	this.tile_livezone = [this.tile_size[0], this.tile_size[1] / 2 - this.tile_deadzone[1]];
 
 	this.delta_x = [this.tile_size[0], 0];
 	this.delta_y = [-this.tile_size[0] / 2, this.tile_size[1] - this.tile_deadzone[1]];
-	
-
 };
 
 HexGrid.prototype.get_tile_position = function(point)
@@ -30,7 +28,7 @@ HexGrid.prototype.get_tile_position = function(point)
 	var highp = [0, 0];
 	
 	// Localizing the y-range
-	if (point[1] <= origin[1]) // North of zero
+	if (point[1] <= origin[1]) // North of [0, 0]
 	{
 		while (high[1] > point[1] + this.delta_y[1])
 		{
@@ -61,7 +59,80 @@ HexGrid.prototype.get_tile_position = function(point)
 		}
 	}
 	
-	return {low: lowp, high: highp};
+	var left = 'h';
+	if (point[0] <= low[0]) // Point os to the left of [0, y]
+	{
+		while (low[0] > point[0] + this.delta_x[0])
+		{
+			low[0] -= this.delta_x[0];
+			lowp[0]--;
+			high[0] -= this.delta_x[0];
+			highp[0]--;
+		}
+		if (low[0] > point[0] + this.delta_x[0] / 2)
+		{
+			low[0] -= this.delta_x[0];
+			lowp[0]--;
+			left = 'l';
+		}
+	}
+	else
+	{
+		while (high[0] < point[0] - this.delta_x[0])
+		{
+			low[0] += this.delta_x[0];
+			lowp[0]++;
+			high[0] += this.delta_x[0];
+			highp[0]++;
+		}
+		if (high[0] < point[0] - this.delta_x[0] / 2)
+		{
+			high[0] += this.delta_x[0];
+			highp[0]++;
+			left = 'l';
+		}
+	}
+
+	// Livezone checks (spray and pray)
+	if (point[1] < low[1] + this.tile_livezone[1])
+	{
+		return lowp;
+	}
+	else if (point[1] > high[1] - this.tile_livezone[1])
+	{
+		return highp;
+	}
+	else
+	{
+		// Livezone miss, deadzone checks (sniper time)
+		if (left === 'l')
+		{
+			var deadzone = [low[0], low[1] + this.tile_deadzone[1]];
+			
+			point[0] -= deadzone[0];
+			point[1] -= deadzone[1];
+			
+			if (point[0] < (this.tile_deadzone[0] / this.tile_deadzone[1]) * (this.tile_deadzone[1] - point[1]))
+				return lowp;
+			else
+				return highp;
+		}
+		else
+		{
+			var deadzone = [high[0], low[1] + this.tile_deadzone[1]];
+			
+			point[0] -= deadzone[0];
+			point[1] -= deadzone[1];
+			
+			if (point[1] < (this.tile_deadzone[1] / this.tile_deadzone[0]) * point[0])
+				return lowp;
+			else
+				return highp;
+		}
+		
+		
+		return [0,0];
+	}
 };
 
 HexGrid.prototype.get_pixel_coordinate = function(position)
@@ -138,7 +209,7 @@ HexGrid.prototype.is_inside = function(position)
 		mark = sprites[1];
 		grid = new HexGrid([16, 16], 8, 3, [16, 16], 'horizontal');
 		
-		select = [[0,0],[0,0]];
+		select = [0,0];
 		
 		$('#game').click(function(event)
 		{
@@ -147,8 +218,7 @@ HexGrid.prototype.is_inside = function(position)
 			position[0]--;
 			position[1]--;
 			
-			point = grid.get_tile_position(position);
-			select = [point.low, point.high];
+			select = grid.get_tile_position(position);;
 		});
 		
 		setInterval(function()
@@ -162,9 +232,7 @@ HexGrid.prototype.is_inside = function(position)
 						gl.draw_sprite(tile, 0, c[0], c[1]);
 				}
 			}
-			var c = grid.get_pixel_coordinate(select[0]);
-			gl.draw_sprite(mark, 0, c[0], c[1]);
-			c = grid.get_pixel_coordinate(select[1]);
+			var c = grid.get_pixel_coordinate(select);
 			gl.draw_sprite(mark, 0, c[0], c[1]);
 			
 			
