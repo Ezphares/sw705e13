96ec17e40b5dc14a3ffeebbc770ccaa61a76f33a
Simon Jensen <mail@simonjensen.net>
2013-12-13 10:20:23 +0100

    Added text above draggable objects.

--- cells/static/js/drag.class.js
+++ cells/static/js/drag.class.js
@@ -8,6 +8,13 @@ Drag = function()
 
 Drag.prototype.draw = function(gl)
 {
+	gl.draw_text("Move", 'black', 15, 0, 32, 10);
+	gl.draw_text("Split", 'black', 15, 0, 105, 10);
+	gl.draw_text("Look", 'black', 15, 0, 175, 10);
+	gl.draw_text("Variable", 'black', 15, 0, 234, 10);
+	gl.draw_text("If", 'black', 15, 0, 326, 10);
+	gl.draw_text("Loop", 'black', 15, 0, 385, 10);
+
 	gl.draw_sprite(Editor.set['move'], 0, paddingX, paddingY);
 	gl.draw_sprite(Editor.set['split'], 0, paddingX = paddingX + spacing, paddingY);
 	gl.draw_sprite(Editor.set['look'], 0, paddingX = paddingX + spacing, paddingY);
