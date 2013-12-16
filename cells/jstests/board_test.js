b4403903c73c643dc09912b58ea20e82bb25533e
WhyNotZoidberg <anders.vinther1@gmail.com>
2013-12-16 12:00:10 +0100

    Updated unit tests

--- cells/jstests/board_test.js
+++ cells/jstests/board_test.js
@@ -36,12 +36,5 @@
 		board.add_entity(friend);
 		
 		ok(board.isDone(), 'isDone: Succeeds when there is only one cell on the board');
-	});	
-	
-	test('get_friendly_cells', function() {
-		board.add_entity(friend);
-		console.log(board);
-		ok(board.get_friendly_cells() == 1, 'get_friendly_cells: Succeeds when there is exactly one friendly cell');
 	});
-	
 })();
--- cells/jstests/menu_test.js
+++ cells/jstests/menu_test.js
@@ -10,13 +10,4 @@
 			game = new Game();
 		}
 	});
-
-	test('isButtonHit', function() {
-			//Button is not hit
-			/* Since width of sprite is 256, we know that the center (320) + 129 is should not be a button hit */
-			equal(Menu.prototype.isButtonHit(320+129, 240, game.gl), false, 'isButtonHit: Succeed if button is not hit');
-			
-			//Button is hit
-			equal(Menu.prototype.isButtonHit(320, 240, game.gl), true, 'isButtonHit: Succeed if button is hit');
-		});
 })();
